1fbd13096262e198ff4aa006fff0f2953434e289
James Holding <james.holding90@gmail.com>
2020-06-16 23:14:27 -0400

    main content finished for now. Need to add in icons later.

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 $(document).ready(function () {
+  // MOMENT JS FOR CURRENT TIME AND DATE
+  var currentTime = moment().format("dddd MMMM Do YYYY, h:mm:ss a");
 
-    $("#new-search").on("click",function() {
+  //   ON CLICK FUNCTION TO UPDATE PAGE
+  $("#new-search").on("click", function () {
     var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=atlanta,georgia&units=imperial&appid=b0aa3b9d4dd84a5bdf53dfbf2bbd9188"
+      "https://api.openweathermap.org/data/2.5/weather?q=atlanta,georgia&units=imperial&appid=b0aa3b9d4dd84a5bdf53dfbf2bbd9188";
 
     $.ajax({
       url: queryURL,
       method: "GET",
-    }).then(function(response) {
-        console.log(response);
-        var cityName = response.name;
-        var cityTemp = response.main.temp;
-        var cityHumidity = response.main.humidity;
-        var cityWindSpeed = response.wind.speed;
-        // console.log(cityName);
-        $("#main-content").append($("<h1>" + cityName + "</h1>"));
-        $("#main-content").append($("<h3> Temp: " + cityTemp + " °F</h3>"));
-        $("#main-content").append($("<h3> Humidity: " + cityHumidity + "</h3>"));
-        $("#main-content").append($("<h3> Wind Speed: " + cityWindSpeed + "</h3>"));
-    
+    }).then(function (response) {
+      console.log(response);
+      var cityName = response.name;
+      var cityTemp = response.main.temp;
+      var cityHumidity = response.main.humidity;
+      var cityWindSpeed = response.wind.speed;
+      var cityUVLongitude = response.coord.lon;
+      console.log(cityUVLongitude);
+      var cityUVLatitude = response.coord.lat;
+      console.log(cityUVLatitude);
+
+      // console.log(cityName);
+      $("#main-content").append($("<h1>" + cityName + "</h1>"));
+      $("#main-content").append($("<h5>" + currentTime + "</h5>"));
+      $("#main-content").append(
+        $("<h5> Temp: " + cityTemp + " °F</h5>")
+      );
+      $("#main-content").append(
+        $("<h5> Humidity: " + cityHumidity + "%</h5>")
+      );
+      $("#main-content").append(
+        $("<h5> Wind Speed: " + cityWindSpeed + " MPH</h5>")
+      );
+
+      var queryUVURL = "http://api.openweathermap.org/data/2.5/uvi?appid=b0aa3b9d4dd84a5bdf53dfbf2bbd9188" +
+      "&lat=" +
+      cityUVLatitude +
+      "&lon=" +
+      cityUVLongitude;
+
+      console.log(queryUVURL);
+
+      $.ajax({
+        url: queryUVURL,
+        method: "GET",
+      }).then(function (response) {
+        $("#main-content").append(
+          $("<h5> UV Index: " + response.value + "</h5>")
+        );
+      });
     });
-    })
+  });
 });
